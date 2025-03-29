9686b64f018a8cd910b788de4c942ae29e61621c
Alexandra Ledesma <alexandraledesma215@gmail.com>
2025-03-29 15:39:39 +0800

    creating todo list using formcontrol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DisplayquotesComponent } from './displayquotes/displayquotes.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TodolistComponent } from "./todolist/todolist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DisplayquotesComponent], // ✅ Required for standalone components
+  imports: [TodolistComponent], // ✅ Required for standalone components
   templateUrl: './app.component.html', // ✅ External HTML template
   styleUrls: ['./app.component.css']
 })
